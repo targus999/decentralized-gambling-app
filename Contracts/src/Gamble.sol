d6ee896beee17bca4e31d30e38d5f1bc6f25d1a0
targus999 <meetakshay.gopal@gmail.com>
2024-12-11 23:36:49 +0530

    added logic for coin and dice

--- Contracts/src/Gamble.sol
+++ Contracts/src/Gamble.sol
@@ -1,14 +1,53 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 contract Gamble {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    enum State{
+        Open,
+        Calculating
     }
 
-    function increment() public {
-        number++;
+
+    State public currentState;
+
+
+    error Gamble__contract_is_busy();
+    error Gamble__send_more_eth();
+
+    
+
+    function rollDice(uint256 bet) public payable validRequest {
+        currentState = State.Calculating;
+        //VRF CALL
+        uint256 randomNumber = 5;
+        if(bet ==randomNumber){
+            withdraw();
+        }
+        currentState = State.Open;
+    }
+
+
+    function flipCoin(uint256 bet) public payable validRequest {
+        currentState = State.Calculating;
+        //VRF CALL
+        uint256 randomNumber = 1;
+        if(bet == randomNumber){    
+            withdraw();
+        }
+        currentState = State.Open;
+    }
+    function withdraw() private {
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success, "Withdrawal failed");
+    }
+
+    modifier validRequest() {
+        if(msg.value != 5e10){
+            revert Gamble__send_more_eth();
+        }
+        if(currentState == State.Calculating){
+            revert Gamble__contract_is_busy();
+        }
+        _;
     }
 }
