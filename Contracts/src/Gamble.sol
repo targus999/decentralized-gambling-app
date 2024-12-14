a7a01ea5d797eabeb7bf66c4b4f5f1cc8757b124
targus999 <meetakshay.gopal@gmail.com>
2024-12-14 13:01:28 +0530

    added events and mminor changes

--- Contracts/src/Gamble.sol
+++ Contracts/src/Gamble.sol
@@ -1,37 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 contract Gamble {
-    enum State{
+    enum State {
         Open,
-        Calculating
+        Dice_Rolling,
+        Coin_Flipping
     }
 
-
     State public currentState;
+    uint256 private immutable minimumBet;
 
+    event BetPlaced(
+        address indexed player,
+        uint256 bet,
+        State gameType,
+        uint256 currentBalance,
+        uint256 timestamp
+    );
 
     error Gamble__contract_is_busy();
     error Gamble__send_more_eth();
 
-    
+    constructor(uint256 minBet) {
+        minimumBet = minBet;
+    }
 
-    function rollDice(uint256 bet) public payable validRequest {
-        currentState = State.Calculating;
+    function rollDice(uint256 bet) external payable validRequest {
+        currentState = State.Dice_Rolling;
+        emit BetPlaced(
+            msg.sender,
+            bet,
+            currentState,
+            address(this).balance,
+            block.timestamp
+        );
         //VRF CALL
         uint256 randomNumber = 5;
-        if(bet ==randomNumber){
+        if (bet == randomNumber) {
             withdraw();
         }
         currentState = State.Open;
     }
 
-
-    function flipCoin(uint256 bet) public payable validRequest {
-        currentState = State.Calculating;
+    function flipCoin(uint256 bet) external payable validRequest {
+        currentState = State.Coin_Flipping;
+        emit BetPlaced(
+            msg.sender,
+            bet,
+            currentState,
+            address(this).balance,
+            block.timestamp
+        );
         //VRF CALL
         uint256 randomNumber = 1;
-        if(bet == randomNumber){    
+        if (bet == randomNumber) {
             withdraw();
         }
         currentState = State.Open;
@@ -42,10 +65,10 @@ contract Gamble {
     }
 
     modifier validRequest() {
-        if(msg.value != 5e10){
+        if (msg.value >= minimumBet) {
             revert Gamble__send_more_eth();
         }
-        if(currentState == State.Calculating){
+        if (currentState != State.Open) {
             revert Gamble__contract_is_busy();
         }
         _;
